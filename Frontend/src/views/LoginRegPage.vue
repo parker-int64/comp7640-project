59b30f3ae88effff2fae113d585641d782d45bfe
parker-int64 <huxiaochuan2011@hotmail.com>
2024-03-22 17:29:59 +0800

    Fixed n-message-provider

--- Frontend/src/views/LoginRegPage.vue
+++ Frontend/src/views/LoginRegPage.vue
@@ -2,7 +2,7 @@
 
 import { ref } from 'vue'
 
-import { NH1, NFlex, NLayout, NCard, NButton, NIcon, NForm, NFormItem, NInput } from 'naive-ui'
+import { NH1, NFlex, NLayout, NCard, NButton, NIcon, NForm, NFormItem, NInput, NMessageProvider } from 'naive-ui'
 
 import { useMessage } from "naive-ui"
 
@@ -82,77 +82,79 @@ const handleLogin = () => {
 </script>
 
 <template>
-    <n-layout style="margin-top: auto;">
-        <n-flex justify="center" align="center">
-            <n-card :title="toggleLogin ? 'Sign Up' : 'Sign In'"  size="large">
-                <n-form
-                ref="formRef"
-                :label-width="120"
-                :model="formValue"
-                :rules="rules"
-                size="large"
-                >
-                    <n-form-item  path="user.name" label="Username">
-                        <n-input 
-                        v-model:value="formValue.user.name"
-                        @input="handleUsernameInput" 
-                        placeholder="Enter your username"
-                        @keydown.enter.prevent
-                    />
-                    </n-form-item>
-
-                    <n-form-item path="user.passwd" label="Password">
-                        <n-input
-                            v-model:value="formValue.user.passwd"
-                            type="password"
-                            @input="handlePasswordInput"
-                            @keydown.enter.prevent
-                            placeholder="Enter your password"
-                        />
-                    </n-form-item>
-
-
-                    <n-form-item path="user.repeatPasswd" label="Repeat Your Password" v-if=toggleLogin>
-                        <n-input
-                            v-model:value="formValue.user.repeatPasswd"
-                            type="password"
-                            @input="handleRepeatPasswd"
+    <n-message-provider>
+        <n-layout style="margin-top: auto;">
+            <n-flex justify="center" align="center">
+                <n-card :title="toggleLogin ? 'Sign Up' : 'Sign In'"  size="large">
+                    <n-form
+                    ref="formRef"
+                    :label-width="120"
+                    :model="formValue"
+                    :rules="rules"
+                    size="large"
+                    >
+                        <n-form-item  path="user.name" label="Username">
+                            <n-input 
+                            v-model:value="formValue.user.name"
+                            @input="handleUsernameInput" 
+                            placeholder="Enter your username"
                             @keydown.enter.prevent
                         />
-                    </n-form-item>
-
-                    <n-form-item v-if=!toggleLogin>
-                        <n-flex justify="space-between" align="center" style="min-width: 100%;">
-                            <n-button @click="handleLogin">
-                                <template #icon>
-                                    <n-icon>
-                                        <log-in-outline />
-                                    </n-icon>
-                                </template>
-                                Login
-                            </n-button>
-                            <n-button text @click="switchLogin">
-                                No account? Sign Up
-                            </n-button>
-                        </n-flex>
-                    </n-form-item>
-
-
-                    <n-form-item v-if=toggleLogin>
-                        <n-flex justify="space-between" align="center" style="min-width: 100%;">
-                            <n-button @click="handleReg">
-                                Sign Up
-                            </n-button>
-
-                            <n-button text @click="switchLogin">
-                                Already have an account? Sign In
-                            </n-button>
-                        </n-flex>
-                    </n-form-item>
-                </n-form>
-            </n-card>
-        </n-flex>
-    </n-layout>
+                        </n-form-item>
+    
+                        <n-form-item path="user.passwd" label="Password">
+                            <n-input
+                                v-model:value="formValue.user.passwd"
+                                type="password"
+                                @input="handlePasswordInput"
+                                @keydown.enter.prevent
+                                placeholder="Enter your password"
+                            />
+                        </n-form-item>
+    
+    
+                        <n-form-item path="user.repeatPasswd" label="Repeat Your Password" v-if=toggleLogin>
+                            <n-input
+                                v-model:value="formValue.user.repeatPasswd"
+                                type="password"
+                                @input="handleRepeatPasswd"
+                                @keydown.enter.prevent
+                            />
+                        </n-form-item>
+    
+                        <n-form-item v-if=!toggleLogin>
+                            <n-flex justify="space-between" align="center" style="min-width: 100%;">
+                                <n-button @click="handleLogin">
+                                    <template #icon>
+                                        <n-icon>
+                                            <log-in-outline />
+                                        </n-icon>
+                                    </template>
+                                    Login
+                                </n-button>
+                                <n-button text @click="switchLogin">
+                                    No account? Sign Up
+                                </n-button>
+                            </n-flex>
+                        </n-form-item>
+    
+    
+                        <n-form-item v-if=toggleLogin>
+                            <n-flex justify="space-between" align="center" style="min-width: 100%;">
+                                <n-button @click="handleReg">
+                                    Sign Up
+                                </n-button>
+    
+                                <n-button text @click="switchLogin">
+                                    Already have an account? Sign In
+                                </n-button>
+                            </n-flex>
+                        </n-form-item>
+                    </n-form>
+                </n-card>
+            </n-flex>
+        </n-layout>
+    </n-message-provider>
 
 </template>
 
